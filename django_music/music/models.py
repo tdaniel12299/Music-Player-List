ffcd9be0f6ae01bd00687920631250fb7bdb701c
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-09-26 23:58:40 -0400

    first commit

--- django_music/music/models.py
+++ django_music/music/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    upload_date = models.DateTimeField(default=timezone.now)
+    song = models.FileField(upload_to='')
+
+    def __str__(self):
+        return self.title
