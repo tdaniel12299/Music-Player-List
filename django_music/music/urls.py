4f142d0dd55e7f1839ad534a6f4deebe946b16e6
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-09-29 20:03:41 -0400

    music update quote added to extend session

--- django_music/music/urls.py
+++ django_music/music/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('',MusicListView.as_view(), name="music-list"),
+    path('', MusicListView.as_view(), name="music-list"),
     path('search',views.search, name="search"),
-    path('create',MusicCreateView.as_view(),name="music-create"),
+    path('create', MusicCreateView.as_view(),name="music-create"),
     path('music_details/<int:pk>/',MusicDetailView.as_view(),name="music-details"),
     path('update/<int:pk>/',views.music_update,name="music-update"),
     path('delete/<int:pk>/',MusicDeleteView.as_view(),name="music-delete"),
