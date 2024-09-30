72cf3eab950f45831b64225903a22600d8ddfeda
Tyler D <130164977+tdaniel12299@users.noreply.github.com>
2024-09-29 22:29:30 -0400

    music list functional: "s" removed from music -details in urls and music list

--- django_music/music/urls.py
+++ django_music/music/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', MusicListView.as_view(), name="music-list"),
     path('search',views.search, name="search"),
     path('create', MusicCreateView.as_view(),name="music-create"),
-    path('music_details/<int:pk>/',MusicDetailView.as_view(),name="music-details"),
+    path('music_details/<int:pk>/',MusicDetailView.as_view(),name="music-detail"),
     path('update/<int:pk>/',views.music_update,name="music-update"),
     path('delete/<int:pk>/',MusicDeleteView.as_view(),name="music-delete"),
 
